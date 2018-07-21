e2cba83f2f2d63bde3a17fd6473a039e9c315aa6
halccw <ccwang.bw@gmail.com>
2018-07-21 11:03:27 -0700

    customize

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ app.use(cors());
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello World from prod server!')
 });
 
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+    {"id": 1,"firstName":"Prod Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Prod Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Prod Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
   ]);
 });
 
